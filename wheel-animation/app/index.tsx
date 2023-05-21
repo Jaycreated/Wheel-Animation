11382c25d2f19ff1d84e451e0b6a06f39e4bd19e
Jay <johnsonjohnadakole@gmail.com>
2023-05-21 01:19:40 +0100

    new

---
+++ wheel-animation/app/index.tsx
@@ -0,0 +1,15 @@
+import FiCamera from 'react-icons/fi';
+import BiVolumeMute from 'react-icons/bi';
+import BiCurrentLocation from 'react-icons/bi';
+import IoWaterOutline from 'react-icons/io'
+
+const App = () => {
+    return (
+        <>
+                <div>
+
+                </div>
+        </>
+
+    )
+}
