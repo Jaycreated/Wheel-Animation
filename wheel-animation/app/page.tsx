3633a99455f5ddebf28f1fbb883854989b9131ab
Jay <johnsonjohnadakole@gmail.com>
2023-06-12 15:16:45 +0100

    push

--- wheel-animation/app/page.tsx
+++ wheel-animation/app/page.tsx
@@ -1,4 +1,4 @@
-
+'use client'
 import { FiCamera } from 'react-icons/fi';
 import { BiVolumeMute, BiCurrentLocation } from 'react-icons/bi';
 import { MdOutlineWaterDrop } from 'react-icons/md';
@@ -7,44 +7,65 @@ import{AiOutlineSound} from 'react-icons/ai';
 import { motion } from 'framer-motion'
 
 const App = () => {
+
+    const wheelVariants = {
+        hidden: { rotate: 0 },
+        visible: { rotate: -405 },
+    };
+
+    const buttonVariants = {
+        hover: { scale: 1.1 },
+    };
+
     return (
         <>
-            <div className="flex min-h-screen  p-24 flex space-x-6 text-sm">
-            <div className='w-full space-y-8 justify-center items-center'>
-                <div className='space-y-10 p-10'>
-                  <div className='flex space-x-6'>
-                    <div className='flex space-x-4 items-center '>
-                        <button className='p-6 bg-gray-900 rounded-full '>
-                            <FiCamera className='text-white ' />
-                        </button>
-                        <p>High resolution <br/> Camera</p>
-                    </div>
-                    <div className="flex items-center space-x-4">
-                        <button className='p-6 bg-gray-900 rounded-full '>
-                            <BiVolumeMute className="" style={{ fill: 'white' }} />
-                        </button>
-                        <p className="ml-2">Noise Cancellation <br/> technology</p>
-                    </div>
-                </div>
+            <div className="flex items-center min-h-screen p-24 flex space-x-6 text-sm">
+                <div className='w-1/2  p-10'>
+                    <div className="flex justify-center items-center relative">
+                        <div className='absolute'>
+                            <div className="relative">
+                                <div className="w-80 h-80 bg-gray-300 rounded-full">
 
-                <div className='flex space-x-6'>
-                    <div className="flex items-center space-x-4 ">
-                        <button className='p-6 bg-gray-900 rounded-full '>
-                            <BiCurrentLocation className='text-white'/>
-                        </button>
-                        <p>GPS navigation <br/> system</p>
-                    </div>
-                    <div className="flex items-center space-x-4">
-                        <button className='p-6 bg-gray-900 rounded-full '>
-                            <MdOutlineWaterDrop className='text-white'/>
-                        </button>
-                        <p>Waterproof design</p>
+                                </div>
+                                <div className="absolute inset-10 w-60 h-60 bg-white rounded-full"></div>
+                            </div>
+
+                        </div>
+                        <div className='absolute w-full h-60 flex justify-center items-center  '>
+                            <div className='w-full'>
+                                <div className='flex justify-between rotate-45'>
+                                    <div>
+                                        <button>
+                                            <RiCameraLensLine />
+                                        </button>
+                                    </div>
+
+                                    <div>
+                                        <button>
+                                            <AiOutlineSound />
+                                        </button>
+                                    </div>
+                                </div>
+                                <div className='flex justify-between'>
+                                    <div>
+                                        <button>
+                                            <BiCurrentLocation />
+                                        </button>
+                                    </div>
+                                    <div>
+                                        <button>
+                                            <MdOutlineWaterDrop />
+                                        </button>
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-            </div>
                 {/*blurred texts*/}
-                <div className='space-y-4 w-full'>
+                <div className='space-y-4 w-full bg-blue-800'>
                     <div className='flex space-x-2 text-sm'>
                     <span className='bg-gray-900 w-64 flex space-x-4 rounded-md p-2 text-white '>
                         <span className='flex justify-center items-center'>
